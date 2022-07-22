f4c9a039dc99064ddf28f4be845eb35fb6a4882d
Gabriel Mesquita <90516048+Gabriel-Mesq@users.noreply.github.com>
2022-07-22 04:42:52 -0300

    Added a Countdown Timer
    Material de apoio: https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Desktop_Widget_Timer.py

--- pomodoro.py
+++ pomodoro.py
@@ -1,23 +1,53 @@
 import pyautogui as ag
+import PySimpleGUI as sg
 import time
+
+sg.theme('Black')
+
+layout = [[sg.Text('Pomodoro Timer:')],
+          [sg.Text('', size=(8, 2), font=('Helvetica', 20),
+                justification='center', key='text')],
+          [sg.Exit(button_color=('white', 'firebrick4'), key='Exit')]]
+
+window = sg.Window('Running Timer', layout,
+                   no_titlebar=True,
+                   auto_size_buttons=False,
+                   keep_on_top=True,
+                   grab_anywhere=True,
+                   element_padding=(0, 0),
+                   finalize=True,
+                   element_justification='c')
+
+def Pomodoro(time_sec):
+    while time_sec:
+        
+        event, values = window.read(timeout=10)
+        mins, secs = divmod(time_sec, 60)
+        current_time = '{:02d}:{:02d}'.format(mins, secs)
+        time.sleep(1)
+        time_sec -= 1
+
+        if event in (sg.WIN_CLOSED, 'Exit'):      
+            break
+
+        window['text'].update(current_time)
+                                                                                            
+    window.close()
 
 while True:
    
     for i in range(4):
         
-        print("Ciclo = ", i)
-
         ag.alert('Confirme para iniciar sua sessão de pomodoro.\n25 minutos de foco')
-        time.sleep(1)
+        Pomodoro(25)
    
         if i < 3:
             ag.alert('Confirme para inciar sua sessão de desacanso.\n5 minutos de intervalo')
-            time.sleep(1)
+            Pomodoro(5)
         else:
             ag.alert('Confirme para inciar sua sessão de desacanso extendido.\n15 minutos de intervalo')
-            time.sleep(1)
+            Pomodoro(15)
             
-
 '''
 focused word: 25 min
 brek: 5 min
